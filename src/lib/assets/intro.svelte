6d88e3eb2ebbe6d6aa2385c0b0df7c40fe4a3ebf
Celso Junio <88014260+junio1k@users.noreply.github.com>
2024-08-28 23:49:33 -0300

    criacao de componentes e conteudo dinamico

---
+++ src/lib/assets/intro.svelte
@@ -0,0 +1,50 @@
+<script>
+    import imgIntroducao from "$lib/assets/introducao.jpg";
+    import icon from "$lib/assets/introducao.jpg"; 
+
+    export let tittle;
+    export let imgIntro;
+    if (imgIntro === "introducao") imgIntro = imgIntroducao;
+    export let href; 
+    export let btn; 
+</script>
+
+
+<section class="intro" style="background-image: url({imgIntro});">
+    <div class="intro__wrapper wrap">
+        <h1>
+            {tittle}
+        </h1>
+        <p>
+            Onde você encontra a maior e mais completa linha de produtos para elevar seu nível.
+        </p>
+        <a class="btn__sobre" href= {href}>{btn}</a>
+    </div>
+</section>
+
+<style>
+    .intro{
+        min-height: 360px;
+        background-size: cover;
+        background-position: center;
+
+        display: grid;
+        align-items: center;
+        justify-content: start;
+        position: relative;
+        
+    }
+    .intro::after {
+        content: "";
+        position: absolute;
+        inset: 0;
+        background-color: rgba(0,0,0,0.40);
+        
+    }
+
+    .intro__wrapper {
+    z-index: 1;
+    max-width: 600px;
+  }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,11 @@
 <script>
-    import imgIntroducao from "$lib/assets/introducao.jpg";
-    import icon from "$lib/assets/introducao.jpg"; 
+    import Intro from "$lib/assets/intro.svelte";
 </script>
 
-
-<section class="intro" style="background-image: url({imgIntroducao});">
-    <div class="intro__wrapper wrap">
-        <h1>
-            Estilo e performance, monte seu PC e arrebente em seus jogos
-        </h1>
-        <p>
-            Onde você encontra a maior e mais completa linha de produtos para elevar seu nível.
-        </p>
-        <a class="btn__sobre" href="">sobre</a>
-    </div>
-</section>
-
-<style>
-    .intro{
-        min-height: 360px;
-        background-size: cover;
-        background-position: center;
-
-        display: grid;
-        align-items: center;
-        justify-content: start;
-        position: relative;
-        
-    }
-    .intro::after {
-        content: "";
-        position: absolute;
-        inset: 0;
-        background-color: rgba(0,0,0,0.40);
-        
-    }
-
-    .intro__wrapper {
-    z-index: 1;
-    max-width: 600px;
-  }
-
-</style>
+<Intro
+tittle = "Estilo e performance, monte seu PC e arrebente em seus jogos"
+imgIntro = "introducao"
+href = "#"
+btn = "Sobre"
+>   
+</Intro>
